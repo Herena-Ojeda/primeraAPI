0b46330086ace7b3ac95418304e4eda94d6b4ef2
Herena <herenaojeda@gmail.com>
2024-10-16 12:51:31 +0200

    feat: configurado servidor de archivos estáticos

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
 import Express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = Express();
 const port = 3000;
 
+const publicPath = path.join(__dirname, '../public');
+app.use(Express.static(publicPath));
+
 app.get('/', (req: Express.Request, res: Express.Response) => {
-    res.send('Hello World!');
+    const targetFilePath = path.join(publicPath, '/index.html');
+    res.sendFile(targetFilePath);
 })
 
 app.listen(port, () => {  
