70d3f1128ec305fc01a1b84a0b1bea974d161e61
Herena <herenaojeda@gmail.com>
2024-10-29 11:17:40 +0100

    cambio en los errores

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,15 @@
 import { saveNewUser } from "../models/userModel.js";
 
 export async function newUser(data: any):Promise<any>{
-    const result = await saveNewUser(data);
-    return result;
+    try {
+        const result = await saveNewUser(data);
+        return result;
+    } catch (error:any){
+        if (error.code === "23505") {
+            const columnMatch = error.detail.match(/Key \((.*?)\)=/);
+            const columnName = columnMatch ? columnMatch[1] : 'campo';
+            return `El ${columnName} ya existe en la base de datos`;
+        }
+        return error;
+    }
 }
